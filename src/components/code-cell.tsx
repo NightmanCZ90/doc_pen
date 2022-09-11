1a2e80cc8af4277dee7aa1812a4bdb3f5fe4e20b
NightmanCZ90 <novak.jan90@gmail.com>
2022-09-11 19:28:20 +0200

    fix: Visible errors from timeouts and bundling

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -7,12 +7,14 @@ import bundle from '../bundler';
 
 const CodeCell = () => {
   const [code, setCode] = useState('');
+  const [err, setErr] = useState('');
   const [input, setInput] = useState('');
 
   useEffect(() => {
     const timer = setTimeout(async () => {
       const output = await bundle(input);
-      setCode(output);
+      setCode(output.code);
+      setErr(output.err);
     }, 1000);
 
     return () => {
@@ -29,7 +31,7 @@ const CodeCell = () => {
             onChange={value => setInput(value)}
           />
         </Resizable>
-        <Preview code={code} />
+        <Preview code={code} err={err} />
       </div>
     </Resizable>
   );
