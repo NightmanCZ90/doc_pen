0a4f69605c264540a59b01f1c735ef058514f65b
NightmanCZ90 <novak.jan90@gmail.com>
2022-09-12 18:58:59 +0200

    feat: Add CodeCell and TextEditor in CellListItem

--- src/hooks/use-typed-selector.ts
+++ src/hooks/use-typed-selector.ts
@@ -1,5 +1,4 @@
 import { TypedUseSelectorHook, useSelector } from 'react-redux';
 import { RootState } from '../state';
 
-
 export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './state';
 
+import { store } from './state';
 import CellList from './components/cell-list';
 
 const root = ReactDOM.createRoot(
