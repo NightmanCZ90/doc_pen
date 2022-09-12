25ea63922ba79efd4af7428615bf7efabb26900d
NightmanCZ90 <novak.jan90@gmail.com>
2022-09-12 19:18:53 +0200

    refactor: Extract state from Text Cell

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,12 +1,18 @@
 import { useEffect, useRef, useState } from 'react';
 import MDEditor from '@uiw/react-md-editor';
 
+import { useActions } from '../hooks/use-actions';
+import { Cell } from '../state';
 import './text-editor.css';
 
-const TextEditor: React.FC = () => {
+interface TextEditorProps {
+  cell: Cell;
+}
+
+const TextEditor: React.FC<TextEditorProps> = ({ cell }) => {
   const ref = useRef<HTMLDivElement | null>(null);
   const [editing, setEditing] = useState(false);
-  const [value, setValue] = useState('# Header');
+  const { updateCell } = useActions();
 
   useEffect(() => {
     const listener = (event: MouseEvent) => {
@@ -25,14 +31,14 @@ const TextEditor: React.FC = () => {
   if (editing) {
     return (
       <div className='text-editor' ref={ref}>
-        <MDEditor value={value} onChange={v => setValue(v || '')} />
+        <MDEditor value={cell.content} onChange={v => updateCell(cell.id, v || '')} />
       </div>
     )
   }
   return (
     <div className='text-editor card' onClick={() => setEditing(true)}>
       <div className="card-content">
-        <MDEditor.Markdown source={value} />
+        <MDEditor.Markdown source={cell.content || 'Click to edit'} />
       </div>
     </div>
   )
